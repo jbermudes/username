9746994b510b3d5c748510ce700e0a18901bc54c
Jess Bermudes <jbermudes@gmail.com>
2020-11-13 10:56:09 -0800

    Update rules

--- username.sh
+++ username.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 # username.sh
 # author
-echo "Enter a username: "
+echo "Enter a username (lowercase, digits, underscore, must start with letter)"
 read -r NAME
-while echo "$NAME" | grep -E -v "^[a-z]{5}$" > /dev/null 2>&1
+while echo "$NAME" | grep -E -v "^[a-z][a-z0-9\_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid username."
+	echo "You must enter a valid username. (lowercase, digits, underscore, must start with letter"
 	echo "Enter a username: "
 	read -r NAME
 done
